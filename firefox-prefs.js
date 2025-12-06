7b34b37886f548de5b5768c60355c6d0a9cfbfb8
goyo123321a <goyo123321@mail.fresnostate.edu>
2025-12-06 21:43:56 +0800

    Create firefox-prefs.js

---
+++ firefox-prefs.js
@@ -0,0 +1,12 @@
+user_pref("browser.startup.homepage", "https://idx.google.com");
+user_pref("browser.startup.page", 1);
+
+user_pref("extensions.autoRefreshPage.enabled", true);
+user_pref("extensions.experiments.enabled", true);
+
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("browser.shell.checkDefaultBrowser", false);
+
+user_pref("gfx.webrender.all", false);
+user_pref("layers.acceleration.force-enabled", false);
